74c265ddf19d68ff26b85b2f309dcdd786cd8ce7
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-05-27 09:08:56 -0300

    Add project files.

---
+++ ControleDeContatos/Controllers/ContatoController.cs
@@ -0,0 +1,55 @@
+﻿using ControleDeContatos.Models;
+using ControleDeContatos.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ControleDeContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        private readonly IContatoRepository _contatoRepository;
+        public ContatoController(IContatoRepository contatoRepository)
+        {
+            _contatoRepository = contatoRepository;
+        }
+        public IActionResult Index()
+        {
+            List<ContatoModel> contatos = _contatoRepository.BuscarTodos();
+
+            return View(contatos);
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar(int id)
+        {
+            ContatoModel contato = _contatoRepository.ListarPorId(id);
+            return View(contato);
+        }
+        public IActionResult ApagarConfirmacao(int id)
+        {
+            ContatoModel contato = _contatoRepository.ListarPorId(id);
+            return View(contato);
+        }
+
+        public IActionResult Apagar(int id)
+        {
+            _contatoRepository.Apagar(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Criar(ContatoModel contato)
+        {
+            _contatoRepository.Adicionar(contato);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult Alterar(ContatoModel contato)
+        {
+            _contatoRepository.Atualizar(contato);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ControleDeContatos/Repository/IContatoRepository.cs
@@ -0,0 +1,14 @@
+﻿using ControleDeContatos.Models;
+using System.Collections.Generic;
+
+namespace ControleDeContatos.Repository
+{
+    public interface IContatoRepository
+    {
+        ContatoModel ListarPorId(int id);
+        List<ContatoModel> BuscarTodos();
+        ContatoModel Adicionar(ContatoModel contato);
+        ContatoModel Atualizar(ContatoModel contato);
+        bool Apagar(int id);
+    }
+}
